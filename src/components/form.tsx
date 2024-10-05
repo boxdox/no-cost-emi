be78ee0426f62b178d40c3d547acacb218b311f7
vaibhav kandwal <19517445+boxdox@users.noreply.github.com>
2024-10-05 14:54:41 +0000

    allow decimal values in input fields

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -6,12 +6,19 @@ export const Form = ({ handleSubmit }: { handleSubmit: (e: Event) => void }) => 
       <h1 class="text-center text-3xl font-bold underline decoration-primary underline-offset-2">
         no cost emi calculator
       </h1>
-      <Input name="amount" label="Amount" min="1" required />
+      <Input 
+        name="amount"
+        label="Amount"
+        min="1"
+        step=".01"
+        required
+      />
       <Input
         name="interest"
         label="Interest"
         subscript="(per annum)"
-        min="1"
+        min=".01"
+        step=".01"
         required
       />
       <Input
@@ -26,9 +33,16 @@ export const Form = ({ handleSubmit }: { handleSubmit: (e: Event) => void }) => 
         label="Processing Fees"
         subscript="(if any)"
         min="0"
+        step=".01"
         defaultValue="0"
       />
-      <Input name="gst" label="GST" min="0" defaultValue="18" />
+      <Input 
+        name="gst" 
+        label="GST" 
+        min="0"
+        step=".01"
+        defaultValue="18" 
+      />
 
       <button
         class="bg-gradient-to-r from-teal-200 to-lime-200 px-5 py-2.5 text-center font-semibold text-gray-900 transition-all hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:outline-none focus:ring-4 focus:ring-lime-200 dark:focus:ring-teal-700"
